1f7b5d932fb0f09402deb3e2249c63bae5333675
neviller666 <n920097@gmail.com>
2023-05-20 16:28:19 -0500

    updateahora2

--- src/database/Conexion.java
+++ src/database/Conexion.java
@@ -1,8 +1,33 @@
 
 package database;
 
+import java.sql.Connection;
+
+
 public class Conexion {
     
+    private final String DRIVER="com.mysql.jdbc.Driver";
+    private final String URL="jdbc:mysql://localhost:3306";
+    private final String DB="dbsistema";
+    private final String USER="root";
+    private final String PASSWORD="";
     
+    public Connection cadena;
+    
+    public Conexion(){
+        this.cadena=null;    
+    }        
+    
+    public Connection conectar(){
+    
+    
+    
+    }
+    
+    public void desconectar(){
+    
+    
+    
+    }
     
 }
